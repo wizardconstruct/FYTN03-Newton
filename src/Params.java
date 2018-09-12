f5b0fd38eaf160bd5b6ec0702df014210ee6cde8
V Woerdings <Vijawoe@gmail.com>
2018-09-12 02:29:08 +0200

    Added more constants

--- src/Params.java
+++ src/Params.java
@@ -4,7 +4,7 @@ public class Params {
 	// Simulation
 	
 	public double h = 1e-2;			// [s]
-	public double v_x_0 = 0.0; 		// Initial velocity in x
+	public double v_x_0 = 3.0; 		// Initial velocity in x
 	public double v_y_0 = 5.0;		// Initial velocity in y
 	
 	// Constants
@@ -12,11 +12,16 @@ public class Params {
 	public double g = 9.81; 		// [m/s]
 	public double eta = 1.8e-5; 	// [kg/(m*s)]
 	
-	public double r = 0.0; 			// [m]
-	public double m = 0.0;			// [kg]
+	public double r = 0.22; 			// [m]
+	public double m = 0.45;			// [kg]
 	public double c = 0.47;			// [-]
 	
 	public double y_0 = 2.0;		// [m]
+        
+        public double rho = 1.225;              // [kg/m^3]        
+        public double Cd = 0.13;                // Used this as source https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3657093/
+        public double A = Math.PI*Math.pow(r, 2);
+        public double B2 = 0.5*Cd*rho*A;
         
         //Experiment
         
