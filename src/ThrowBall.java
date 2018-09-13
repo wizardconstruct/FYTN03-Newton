e682290f85df02d8394ebbf2da8c592ea4631524
V Woerdings <Vijawoe@gmail.com>
2018-09-13 20:13:49 +0200

    Fitted R and t of experiment to the simulation

--- src/ThrowBall.java
+++ src/ThrowBall.java
@@ -7,23 +7,25 @@ public class ThrowBall {
 
 	public static void main(String[] arg){
             
-                int v_steps = 5;
-                double v_min = 12;
-                double v_max = 20;
+                Params params = new Params();
+            
+                int v_steps = 10;
+                double v_min = 14.39;
+                double v_max = 14.49;
                 double v_inc = (v_max-v_min)/(v_steps-1);
                 double [] v_array = new double[v_steps];
                 for(int i = 0; i < v_array.length; i++){
                     v_array[i] = v_min+i*v_inc;
                 }
-                int phi_steps = 5;
-                double phi_min = 15;
-                double phi_max = 40;
+                int phi_steps = 10;
+                double phi_min = 36.8;
+                double phi_max = 37.8;
                 double phi_inc = (phi_max-phi_min)/(phi_steps-1);
                 double [] phi_array = new double[phi_steps];
                 for(int i = 0; i < phi_array.length; i++){
                     phi_array[i] = phi_min+i*phi_inc;
                 }
-                
+                                
                 for(double v : v_array){
                     for(double phi : phi_array){
                         Simulation sim = new Simulation(true, true, phi, v);
@@ -35,6 +37,7 @@ public class ThrowBall {
                                 e.printStackTrace();
                         }
                     }
+                    System.out.println("");
                 }
 /*
                 Params params = new Params();
